8feb37342a2f9574a9279439abdb7bd6066a9818
aymane <uncacademycode@gmail.com>
2026-02-15 22:22:10 +0000

    hope

--- src/screens/Components.jsx
+++ src/screens/Components.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import ProductShowcase from '@/components/sections/ProductSectionOne'
+import ProductSectionOne from '@/components/sections/ProductSectionOne'
 import ProdTwo from '@/components/sections/ProdTwo'
 import ProductThree from '@/components/sections/ProductThree'
 import ProductFour from '@/components/sections/ProductFour'
@@ -13,7 +13,7 @@ function Components() {
       --------------------------------------------------------------------------- */}
       <div className="group relative block w-100 h-75 rounded-2xl border border-white/10 bg-black overflow-hidden hover:border-orange-500/50 hover:shadow-2xl hover:shadow-orange-500/10 transition-all duration-500">
         <div className="absolute top-0 left-0 w-7xl origin-top-left scale-[0.3125] pointer-events-none select-none bg-black h-screen">
-          <ProductShowcase />
+          <ProductSectionOne />
         </div>
 
         <div className="absolute inset-0 bg-linear-to-t from-black/90 via-black/20 to-transparent opacity-60 group-hover:opacity-40 transition-opacity duration-500 z-10" />
